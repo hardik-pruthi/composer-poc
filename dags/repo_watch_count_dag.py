06175fc075ee2b192eabcda6936bfa1dd8fe44db
hardikpruthi <hardikpruthi.pruthi0@gmail.com>
2021-12-13 17:31:47 +0530

    Added requirements.txt and changes in DAG tasks

--- dags/repo_watch_count_dag.py
+++ dags/repo_watch_count_dag.py
@@ -17,20 +17,21 @@ default_dag_args = {
 
 bq_dataset_name = 'analytics-323214.github_commits_dataset'
 bq_github_table_id = bq_dataset_name + '.repo_watchlist_query'
-output_file = 'gs://us-central1-composer-demo-32dddf6c-bucket/watchlist.csv'
+output_file = 'gs://us-central1-composer-demo-32dddf6c-bucket/watchlist' + datetime.datetime.now().strftime(
+    '%Y-%m-%d') + '.csv'
 
 with DAG('demo_dag', schedule_interval=datetime.timedelta(days=1), default_args=default_dag_args,) as dag:
 
-    bq_airflow_commits_query = BigQueryExecuteQueryOperator(
-        task_id='bq_kubernetes_commits_query',
+    github_repo_watchlist_query = BigQueryExecuteQueryOperator(
+        task_id='github_repo_watchlist_query',
         sql="""SELECT repo_name, watch_count FROM [analytics-323214:github_commits_dataset.repo_watchlist]
         where repo_name LIKE "%{{ dag_run.conf["repo_name"] if dag_run.conf.get("repo_name") else "" }}%" and watch_count > 
         {{ dag_run.conf["watch_count"] if dag_run.conf.get("watch_count") else 0 }}  """,
         destination_dataset_table=bq_github_table_id
     )
 
-    export_commits_to_gcs = BigQueryToGCSOperator(
-        task_id='export_kubernetes_commits_to_gcs',
+    export_watchlist_to_gcs = BigQueryToGCSOperator(
+        task_id='export_watchlist_to_gcs',
         source_project_dataset_table=bq_github_table_id,
         destination_cloud_storage_uris=[output_file],
         export_format='CSV'
@@ -41,4 +42,4 @@ with DAG('demo_dag', schedule_interval=datetime.timedelta(days=1), default_args=
         deletion_dataset_table=bq_github_table_id,
     )
 
-    bq_airflow_commits_query >> export_commits_to_gcs >> delete_temp_table
+    github_repo_watchlist_query >> export_watchlist_to_gcs >> delete_temp_table
